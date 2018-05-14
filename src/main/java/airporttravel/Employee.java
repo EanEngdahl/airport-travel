440dc3dd1046b76a566cb79a64ef96fbf7ff12e4
Ean <engdahl.ean@gmail.com>
2018-05-14 15:49:52 -0700

    Added Potential Identity Classes

---
+++ src/main/java/airporttravel/Employee.java
@@ -0,0 +1,26 @@
+package airporttravel;
+
+public class Employee {
+	private String firstName;
+	private String lastName;
+	private int ID;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+}
---
+++ src/main/java/airporttravel/Ticket.java
@@ -0,0 +1,20 @@
+package airporttravel;
+
+public class Ticket {
+	private int cost;
+	private int flightNumber;
+	
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	public int getFlightNumber() {
+		return flightNumber;
+	}
+	public void setFlightNumber(int flightNumber) {
+		this.flightNumber = flightNumber;
+	}
+	
+}
