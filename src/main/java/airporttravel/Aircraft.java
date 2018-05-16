649ecf0029913eb057cba640387642352e4323cd
ean <engdahl.ean@gmail.com>
2018-05-16 12:49:58 -0700

    Added Aircraft object and list

---
+++ src/main/java/airporttravel/Aircraft.java
@@ -0,0 +1,76 @@
+package airporttravel;
+
+public class Aircraft {
+
+	private String name;
+	private String model;
+	private String location;
+	private String heading;
+	private int capacity;
+	private int range;
+	private boolean inFlight;
+	
+	public Aircraft() {
+		this("", "", "", 0, 0, false);
+	}
+	public Aircraft(String name_, String model_, String position_, int capacity_, int range_, boolean inFlight_) {
+		name = name_;
+		model = model_;
+		capacity = capacity_;
+		range = range_;
+		inFlight = inFlight_;
+		if (inFlight)
+		{
+			location = null;
+			heading = position_;
+		}
+		else
+		{
+			location = position_;
+			heading = null;
+		}
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name_) {
+		name = name_;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model_) {
+		model = model_;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location_) {
+		location = location_;
+	}
+	public String getHeading() {
+		return heading;
+	}
+	public void setHeading(String heading_) {
+		heading = heading_;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity_) {
+		capacity = capacity_;
+	}
+	public int getRange() {
+		return range;
+	}
+	public void setRange(int range_) {
+		range = range_;
+	}
+	public boolean isInFlight() {
+		return inFlight;
+	}
+	public void setInFlight(boolean inFlight_) {
+		inFlight = inFlight_;
+	}
+	
+}
---
+++ src/main/java/airporttravel/AircraftList.java
@@ -0,0 +1,65 @@
+package airporttravel;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class AircraftList extends ArrayList<Aircraft>{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public boolean add(Aircraft a_) 
+	{
+		if (isIn(a_.getName()))
+			return false;
+		return super.add(a_);
+	}
+	
+	public Aircraft remove(String name_)
+	{
+		int _index = indexOf(name_);
+		Aircraft _tmp = super.get(_index);
+		
+		if (_index == -1)
+			return null;
+		else
+		{
+			super.remove(_index);
+			return _tmp;
+		}
+	}
+	
+	public boolean isIn(String name_)
+	{
+		Iterator<Aircraft> _itr = this.iterator();
+		Aircraft _tmp;
+		
+		while (_itr.hasNext())
+		{
+			_tmp = _itr.next();
+			if (_tmp.getName() == name_)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public int indexOf(String name_)
+	{
+		Iterator<Aircraft> _itr = this.iterator();
+		Aircraft _tmp;
+		int _count = 0;
+		
+		while (_itr.hasNext())
+		{
+			_tmp = _itr.next();
+			if (_tmp.getName() == name_)
+			{
+				return _count;
+			}
+			_count++;
+		}
+		return -1;
+	}
+}
