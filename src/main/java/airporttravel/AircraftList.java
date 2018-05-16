9c8e34dafbc94ac35ff625db07972734c883a001
brysonbn <bryson.bingham@gmail.com>
2018-05-16 13:37:14 -0700

    changed formatting

--- src/main/java/airporttravel/AircraftList.java
+++ src/main/java/airporttravel/AircraftList.java
@@ -8,38 +8,33 @@ public class AircraftList extends ArrayList<Aircraft>{
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	public boolean add(Aircraft a_) 
-	{
+	public boolean add(Aircraft a_)  {
 		if (isIn(a_.getName()))
 			return false;
 		return super.add(a_);
 	}
 	
-	public Aircraft remove(String name_)
-	{
+	public Aircraft remove(String name_) {
 		int _index = indexOf(name_);
 		Aircraft _tmp = super.get(_index);
 		
-		if (_index == -1)
+		if (_index == -1) {
 			return null;
-		else
-		{
+		} 
+		else {
 			super.remove(_index);
 			return _tmp;
 		}
 	}
 	
-	public int indexOf(String name_)
-	{
+	public int indexOf(String name_) {
 		Iterator<Aircraft> _itr = this.iterator();
 		Aircraft _tmp;
 		int _count = 0;
 		
-		while (_itr.hasNext())
-		{
+		while (_itr.hasNext()) {
 			_tmp = _itr.next();
-			if (_tmp.getName() == name_)
-			{
+			if (_tmp.getName() == name_) {
 				return _count;
 			}
 			_count++;
@@ -47,8 +42,7 @@ public class AircraftList extends ArrayList<Aircraft>{
 		return -1;
 	}
 		
-	public boolean isIn(String name_)
-	{
+	public boolean isIn(String name_) {
 		if(indexOf(name_) == -1) {
 			return false;
 		}
