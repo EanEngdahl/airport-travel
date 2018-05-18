361903aeb2b3fcbc251604fb28e84c150792394c
ean <engdahl.ean@gmail.com>
2018-05-18 13:28:07 -0700

    Aircraft now holds total number of seats and passengers onboard

--- src/main/java/airportsysmodel/Aircraft.java
+++ src/main/java/airportsysmodel/Aircraft.java
@@ -1,17 +1,23 @@
 package airportsysmodel;
 
+import java.math.BigDecimal;
+import java.util.stream.IntStream;
+
 public class Aircraft {
 
 	/* will be size of s, m, l */
 	private char aircraftSize;
 	private AircraftSectionList sectionList;
 	private int maxAircraftSeats;
+	private int totalNumOfPassengers;
 
 		
-	public Aircraft(char aircraftSize_, int seatsFilledPerSection_[]) {
+	public Aircraft(char aircraftSize_, int seatsFilledPerSection_[], 
+			BigDecimal costOfSeat_[], int maxSeatsPerSection_[]) {
 		aircraftSize = aircraftSize_;
-		sectionList = new AircraftSectionList(aircraftSize_, seatsFilledPerSection_);
-		setMaxAircraftSeats();
+		sectionList = new AircraftSectionList(aircraftSize_, seatsFilledPerSection_, costOfSeat_);
+		setMaxAircraftSeats(maxSeatsPerSection_);
+		setTotalNumOfPassengers(seatsFilledPerSection_);
 	}
 
 	public char getAircraftSize() {
@@ -26,26 +32,19 @@ public class Aircraft {
 		return sectionList;
 	}
 
-	public void setSectionList(AircraftSectionList sectionList) {
-		this.sectionList = sectionList;
-	}
-
 	public int getMaxAircraftSeats() {
 		return maxAircraftSeats;
 	}
 
-	public void setMaxAircraftSeats() {
-		switch(aircraftSize) {
-			case 's':
-				maxAircraftSeats = SMALL_PLANE_SEATS;
-				break;
-			case 'm':
-				maxAircraftSeats = MEDIUM_PLANE_SEATS;
-				break;
-			case 'l':
-			default:
-				maxAircraftSeats = LARGE_PLANE_SEATS;
-				break;
-		}
+	public void setMaxAircraftSeats(int maxSeatsPerSection_[]) {
+		maxAircraftSeats = IntStream.of(maxSeatsPerSection_).sum();
+	}
+	
+	public void setTotalNumOfPassengers(int seatsFilledPerSection_[]) {
+		totalNumOfPassengers = IntStream.of(seatsFilledPerSection_).sum();
+	}
+	
+	public int getTotalNumOfPassengers() {
+		return totalNumOfPassengers;
 	}
 }
