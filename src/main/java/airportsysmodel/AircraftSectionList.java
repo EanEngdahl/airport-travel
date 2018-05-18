8b9f958e7073d44a52ce414f77311f9e05e8ebba
ean <engdahl.ean@gmail.com>
2018-05-18 12:59:23 -0700

    Updated AircraftSectionList input

--- src/main/java/airportsysmodel/AircraftSectionList.java
+++ src/main/java/airportsysmodel/AircraftSectionList.java
@@ -1,6 +1,7 @@
 package airportsysmodel;
 
 import java.util.ArrayList;
+import java.math.BigDecimal;
 
 public class AircraftSectionList extends ArrayList<AircraftSection> {
 	private static final long serialVersionUID = 5534735606050325939L;
@@ -8,39 +9,39 @@ public class AircraftSectionList extends ArrayList<AircraftSection> {
 		FIRST, BUSINESS, ECON_PLUS, ECON_BASIC;
 	}
 
-	public AircraftSectionList(char aircraftSize_, int seatsFilledPerSection_[]) {
+	public AircraftSectionList(char aircraftSize_, int seatsFilledPerSection_[], BigDecimal costOfSeat_) {
 		createSections(aircraftSize_, seatsFilledPerSection_);
 	}
 	
-	public void createSections(char aircraftSize_, int seatsFilledPerSection_[]) {
+	public void createSections(char aircraftSize_, int seatsFilledPerSection_[], BigDecimal costOfSeat_) {
 		String _classSectionName;
 		
 		switch(aircraftSize_) {
 		case 's':
 			//create 1 section with proper name
 			_classSectionName = sectionClassTypes.ECON_BASIC.toString();
-			add(new AircraftSection(seatsFilledPerSection_[0], _classSectionName, aircraftSize_));
+			add(new AircraftSection(seatsFilledPerSection_[0], _classSectionName, costOfSeat_));
 			break;
 		case 'm':
 			//create 3 sections with proper names
 			_classSectionName = sectionClassTypes.ECON_BASIC.toString();
-			add(new AircraftSection(seatsFilledPerSection_[0], _classSectionName, aircraftSize_));
+			add(new AircraftSection(seatsFilledPerSection_[0], _classSectionName, costOfSeat_));
 			_classSectionName = sectionClassTypes.BUSINESS.toString();
-			add(new AircraftSection(seatsFilledPerSection_[2], _classSectionName, aircraftSize_));
+			add(new AircraftSection(seatsFilledPerSection_[2], _classSectionName, costOfSeat_));
 			_classSectionName = sectionClassTypes.FIRST.toString();
-			add(new AircraftSection(seatsFilledPerSection_[3], _classSectionName, aircraftSize_));
+			add(new AircraftSection(seatsFilledPerSection_[3], _classSectionName, costOfSeat_));
 			break;
 		case 'l':
 		default:
 			//create 4 sections with proper names
 			_classSectionName = sectionClassTypes.ECON_BASIC.toString();
-			add(new AircraftSection(seatsFilledPerSection_[0], _classSectionName, aircraftSize_));
+			add(new AircraftSection(seatsFilledPerSection_[0], _classSectionName, costOfSeat_));
 			_classSectionName = sectionClassTypes.ECON_PLUS.toString();
-			add(new AircraftSection(seatsFilledPerSection_[1], _classSectionName, aircraftSize_));
+			add(new AircraftSection(seatsFilledPerSection_[1], _classSectionName, costOfSeat_));
 			_classSectionName = sectionClassTypes.BUSINESS.toString();
-			add(new AircraftSection(seatsFilledPerSection_[2], _classSectionName, aircraftSize_));
+			add(new AircraftSection(seatsFilledPerSection_[2], _classSectionName, costOfSeat_));
 			_classSectionName = sectionClassTypes.FIRST.toString();
-			add(new AircraftSection(seatsFilledPerSection_[3], _classSectionName, aircraftSize_));
+			add(new AircraftSection(seatsFilledPerSection_[3], _classSectionName, costOfSeat_));
 			break;
 		}	
 	}
