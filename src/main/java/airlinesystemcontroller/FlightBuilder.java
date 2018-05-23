00110f2857dcdb654e04577b465e0ad16d82dc4f
ean <engdahl.ean@gmail.com>
2018-05-23 11:54:58 -0700

    Removed logic from Aircraft

--- src/main/java/airlinesystemcontroller/FlightBuilder.java
+++ src/main/java/airlinesystemcontroller/FlightBuilder.java
@@ -1,10 +1,12 @@
 package airlinesystemcontroller;
 
 import java.math.BigDecimal;
+import java.util.stream.IntStream;
 
 import airlinesystemmodel.AircraftPilot;
 import airlinesystemmodel.Airport;
 import airlinesystemmodel.Flight;
+import airlinesystemmodel.Aircraft;
 
 public class FlightBuilder {
 	
@@ -14,21 +16,33 @@ public class FlightBuilder {
 	
 		AircraftPilot _pilot;
 		AircraftPilot _coPilot;
-
+		Aircraft _aircraftAssigned;
+		
 		PilotBuilder assignPilots = new PilotBuilder();
 
 		_pilot = assignPilots.assignPilotToAircraft(aircraftSize_);
 		_coPilot = assignPilots.assignPilotToAircraft(aircraftSize_);	
+		_aircraftAssigned = new Aircraft(aircraftSize_, getTotalNumOfPassengers(seatsFilledPerSection_),
+				seatsFilledPerSection_, seatCostPerSection_, getMaxAircraftSeats(maxSeatsPerSection_));
 		Airport source = new Airport(source_);
 		Airport destination = new Airport(distance_);
 		
 		Flight _newFlightFromData = new Flight(aircraftSize_, maxSeatsPerSection_, 
 				seatsFilledPerSection_, seatCostPerSection_, source, destination, 
-				distanceTravelled_, _pilot, _coPilot);
+				distanceTravelled_, _pilot, _coPilot, _aircraftAssigned);
 	
 		return _newFlightFromData;
 		/* TODO: newFlightFromData will go to a database/file storing all the flights for 
 		 * querying etc.
 		 */
+	}
+	
+	public int getTotalNumOfPassengers(int seatsFilledPerSection_[]) {
+		return IntStream.of(seatsFilledPerSection_).sum();
+	}
+	
+	public int getMaxAircraftSeats(int maxSeatsPerSection_[]) {
+		return IntStream.of(maxSeatsPerSection_).sum();
+				
 	}
 }
