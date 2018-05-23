bfafefcb03c88465d40a43f944998e29d7d8492d
ean <engdahl.ean@gmail.com>
2018-05-23 12:25:31 -0700

    Added commenting to FlightRCPManager

--- src/main/java/airlinesystemcontroller/FlightRCPManager.java
+++ src/main/java/airlinesystemcontroller/FlightRCPManager.java
@@ -1,3 +1,9 @@
+/*
+ * FlightRCPManager class
+ *		Calculates the revenue, cost, and overall profit
+ *		for a given flight
+ */
+
 package airlinesystemcontroller;
 
 import java.math.BigDecimal;
@@ -11,12 +17,29 @@ public class FlightRCPManager {
 	private BigDecimal cost;
 	private BigDecimal profit;
 	
+	/*
+	 * Default constructor
+	 */
 	public FlightRCPManager() {}
 	
+	/*
+	 * Constructor calls to find profit of a flight
+	 * 
+	 * @param flightToCalculate_
+	 * 		flight to be passed and used for finding profit
+	 */
 	public FlightRCPManager(Flight flightToCalculate_) {
 		findProfit(flightToCalculate_);
 	}
 	
+	/*
+	 * Calculate and set revenue class variable
+	 * 
+	 * @param flightToCalculate_
+	 * 		flight with information to use for finding revenue
+	 * @return
+	 * 		BigDecimal type revenue to be used for profit calculations
+	 */
 	public BigDecimal findRevenue(Flight flightToCalculate_) {
 		revenue = new BigDecimal("0");
 		int seatsFilledPerSection_[] = flightToCalculate_.getSeatsFilledPerSection();
@@ -29,6 +52,14 @@ public class FlightRCPManager {
 		return revenue;
 	}
 
+	/*
+	 * Calculate and set cost class variable
+	 * 
+	 * @param flightToCalculate_
+	 * 		flight with information to use for finding cost
+	 * @ return
+	 * 		BigDecimal type cost to be used for profit calculations
+	 */
 	public BigDecimal findCost(Flight flightToCalculate_) {
 		cost = new BigDecimal(flightToCalculate_.getDistanceTravelled());
 		cost = cost.multiply(FUELCOST);
@@ -37,6 +68,14 @@ public class FlightRCPManager {
 		return cost;
 	}
 	
+	/*
+	 * Calculate and set profit class variable
+	 * 
+	 * @param flightToCalculate_
+	 * 		flight with information to use for finding profit
+	 * @return
+	 * 		BigDecimal type profit for the given flight
+	 */
 	public BigDecimal findProfit(Flight flightToCalculate_) {
 		profit = new BigDecimal("0");
 		profit = profit.add(findRevenue(flightToCalculate_));
