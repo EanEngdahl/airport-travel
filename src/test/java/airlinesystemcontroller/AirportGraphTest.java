6c91694e7f56d7867b6724f93dfc91e4c5d16962
ean <engdahl.ean@gmail.com>
2018-05-29 08:39:13 -0700

    Added more graph testing

--- src/test/java/airlinesystemcontroller/AirportGraphTest.java
+++ src/test/java/airlinesystemcontroller/AirportGraphTest.java
@@ -2,8 +2,6 @@ package airlinesystemcontroller;
 
 import static org.junit.Assert.*;
 
-import java.util.HashMap;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -32,14 +30,26 @@ public class AirportGraphTest {
 		_graphOfAirports.removeEdge("A", "B");
 		assertFalse("There is no edge from A to B", _graphOfAirports.areAirportsConnected("A", "B"));
 		assertEquals("After removing edge, distance between A and B is 0.0", _graphOfAirports.getDistance("A", "B"), 0.0, 0.01);
+		
+		assertTrue("There should be an edge added from A to D", _graphOfAirports.createEdge("D", "A", 37));
+		assertFalse("There should be no new edge allowed between A and D", 
+				_graphOfAirports.createEdge("A", "D", 12));
+		assertEquals("Distance between A and D should be 37.0", _graphOfAirports.getDistance("A", "D"),
+				37.0, 0.01);
+		_graphOfAirports.removeAirport("D");
+		assertFalse("D should no longer be in the graph", _graphOfAirports.isAirportInGraph("D"));
+		assertFalse("D and A should not be connected", _graphOfAirports.areAirportsConnected("D", "A"));
 	}
 	
+	@Test
 	public void testAirportFunctions() {
 		assertTrue("Airport A is in graph",_graphOfAirports.isAirportInGraph("A"));
 		assertFalse("Airport C is not in graph",_graphOfAirports.isAirportInGraph("C"));
 		_graphOfAirports.addAirport(new Airport("C"));
 		assertTrue("Airport C is now in graph after adding it",_graphOfAirports.isAirportInGraph("C"));
 		
+		assertEquals("Hashmap should return airport with correct name field", "A", 
+				_graphOfAirports.getAirport("A").getName());
+		
 	}
-
 }
