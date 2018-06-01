724871b086544e69da8db356097b53e4dee69b17
ean <engdahl.ean@gmail.com>
2018-06-01 09:49:31 -0700

    Used menu logger instead of System.out

--- src/main/java/airlinesystemview/ConsoleView.java
+++ src/main/java/airlinesystemview/ConsoleView.java
@@ -1,6 +1,8 @@
 package airlinesystemview;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Scanner;
 import java.math.BigDecimal;
 import java.text.NumberFormat;
@@ -8,30 +10,32 @@ import java.text.NumberFormat;
 
 public class ConsoleView {
 	
+	private Logger _menuLogger = LoggerFactory.getLogger("menuLogger");
+
 	public String[] promptUserForFilenames(Logger output_, Scanner input_) {
 		String _fileNames[] = new String [2];
 		String _selection;
 
 		try {
 			do {
-				System.out.println("Input custom file paths. If none are input, use defaults.");
-				System.out.println("1. Enter custom properties file path\n"
+				_menuLogger.info("Input custom file paths. If none are input, use defaults.\n");
+				_menuLogger.info("1. Enter custom properties file path\n"
 						+ "2. Enter custom graph file path\n"
-						+ "3. Return to Main Menu\n");
+						+ "3. Return to Main Menu\n\n");
 			
 				_selection = input_.nextLine();
 				
 				switch(_selection) {
 					case "1":
 					case "2":
-						System.out.print("Input file path: ");
+						_menuLogger.info("Input file path: ");
 						_fileNames[Integer.parseInt(_selection) - 1] = input_.nextLine();
 						break;
 					case "3":
-						System.out.println("Running program");
+						_menuLogger.info("Running program\n");
 						break;
 					default:
-						System.out.println("Input a valid option\n");
+						_menuLogger.info("Input a valid option\n\n");
 				}
 
 			} while(!_selection.equals("3"));
@@ -46,12 +50,12 @@ public class ConsoleView {
 	public int showMainMenu(Logger output_, Scanner input_) {
 		String _selection;
 		
-		System.out.println("MAIN MENU:");
-		System.out.println("1. Input custom files\n"
+		_menuLogger.info("MAIN MENU:\n");
+		_menuLogger.info("1. Input custom files\n"
 				+	 "2. Run simulation\n"
 				+	 "3. Show results\n"
 				+	 "4. Find average profit between airports\n"
-				+	 "0. Quit\n");
+				+	 "0. Quit\n\n");
 
 		try {
 		
@@ -70,7 +74,7 @@ public class ConsoleView {
 						case "4":
 							return 4;
 						default:
-							System.out.println("Input a valid option");
+							_menuLogger.info("Input a valid option\n");
 							break;
 				}
 			}
@@ -83,9 +87,9 @@ public class ConsoleView {
 	public String[] findAverageBetweenAirports(Scanner input_) {
 		String[] _airportNames = new String[2];
 		
-		System.out.print("Input first airport name: ");
+		_menuLogger.info("Input first airport name: ");
 		_airportNames[0] = input_.nextLine().toUpperCase();
-		System.out.print("Input second airport name: ");
+		_menuLogger.info("Input second airport name: ");
 		_airportNames[1] = input_.nextLine().toUpperCase();
 		
 		return _airportNames;
@@ -93,14 +97,14 @@ public class ConsoleView {
 	
 	public void displayAverageBetweenAirports(BigDecimal averageProfit_) {
 		NumberFormat _numberFormatter = NumberFormat.getInstance();
-		System.out.println("The average profit is $" + _numberFormatter.format(averageProfit_) + "\n");
+		_menuLogger.info("The average profit is $" + _numberFormatter.format(averageProfit_) + "\n\n");
 	}
 	
 	public void resultsView(Logger output_, BigDecimal profit_, BigDecimal cost_, 
 			BigDecimal revenue_, int totalFlights_, BigDecimal averageFlightProfit_) {
 		NumberFormat _numberFormatter = NumberFormat.getInstance();
-		System.out.println("\n\n*****Flight Results*****");
-		System.out.println("Total number of flights: " + _numberFormatter.format(totalFlights_)
+		_menuLogger.info("\n\n*****Flight Results*****\n\n");
+		_menuLogger.info("Total number of flights: " + _numberFormatter.format(totalFlights_)
 				+ "\nTotal revenue: " + _numberFormatter.format(revenue_)
 				+ "\nTotal cost: " + _numberFormatter.format(cost_)
 				+ "\nTotal Profit: " + _numberFormatter.format(profit_) 
